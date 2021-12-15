9380c9f0418df518c4620f2db7aefc25a2f129fa
anindarevioktaviani <92665686+anindarevioktaviani@users.noreply.github.com>
2021-12-15 22:30:53 +0700

    Add files via upload

---
+++ posttest4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    
+    cout<< "Nama:Aninda Revi Oktaviani"<<endl;
+    cout<< "npm :2117051099           " <<endl;
+    cout<< "==========================================================" <<endl;
+    
+    int a;
+    int b[a];
+    
+    
+    
+    cout<<" jumlah data=";
+    cin>>a;
+    
+    cout<<"data nilai=";
+    
+    for (int i=0;i<a;i++){
+    	
+    	     cin>>b[i];
+    	
+    	
+	}
+	float c,d ;
+	
+	cout<<endl;
+	for(int i=0;i<a;i++){
+		
+		cout<<b[i]<<endl;
+		c=b[i]+c;
+		
+	}
+	
+	
+	d=c/a;
+	cout<<"RATA RATA="<<d<<endl;
+	
+
+}
